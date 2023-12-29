3b16725f5d31d1c1cbae0f71c8d3658220d5c365
MinhLe2512 <minhdangle2512@gmail.com>
2023-12-29 14:15:11 +0700

    update scene

--- src/main.js
+++ src/main.js
@@ -4,10 +4,20 @@ import Stats from 'three/examples/jsm/libs/stats.module';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { GLTFLoader  } from 'three/addons/loaders/GLTFLoader.js';
 import * as TWEEN from '@tweenjs/tween.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
+import { clone } from 'three/examples/jsm/utils/SkeletonUtils.js';
 
 // let noise = new FastNoiseLite();
 // noise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
-
+const params = {
+  threshold: 0,
+  strength: .15,
+  radius: 0,
+  exposure: 1
+};
 
 function loadFile(filename) {
   return new Promise((resolve, reject) => {
@@ -56,8 +66,6 @@ var tween = new TWEEN.Tween(causticsCamera.position);
 tween.to({x: 5}, 20000).repeat(Infinity);
 
 tween.start();
-
-
 //scene.add(causticsGroup);
 
 const cameraHelper = new THREE.CameraHelper(causticsCamera);
@@ -68,6 +76,7 @@ const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
 renderer.setSize(width, height);
 renderer.autoClear = false;
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild( renderer.domElement );
 
 
@@ -75,19 +84,14 @@ const temporaryRenderTarget = new THREE.WebGLRenderTarget(width, height);
 
 // const spotLight = new THREE.DirectionalLight(0xffffff , 1);
 
-const directLightCaustics = new THREE.HemisphereLight( 0xffffff, 0x080820, 15 );
+const directLightCaustics = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1.8 );
 const spotLight = new THREE.SpotLight( 0xffffff, 200, 25, 0.53, 1, 1 );
 spotLight.position.set( 0, 0, 20 );
-
-
-//spotLight.map = temporaryRenderTarget.texture;
-
-spotLight.castShadow = true;
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
-spotLight.shadow.camera.near = 1;
-spotLight.shadow.camera.far = 10;
-spotLight.shadow.focus = 1;
+// spotLight.shadow.mapSize.width = 1024;
+// spotLight.shadow.mapSize.height = 1024;
+// spotLight.shadow.camera.near = 0.1;
+// spotLight.shadow.camera.far = 100;
+//spotLight.shadow.focus = 1;
 
 
 scene.add(spotLight);
@@ -99,16 +103,19 @@ scene.add(spotLightHelper);
 // Create mouse Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// controls.target = waterPosition;
+const renderScene = new RenderPass( scene, camera );
 
-// controls.minPolarAngle = 0;
-// controls.maxPolarAngle = Math.PI / 2. - 0.1;
+const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1, 0.4, 0.85 );
+bloomPass.threshold = params.threshold;
+bloomPass.strength = params.strength;
+bloomPass.radius = params.radius;
 
-// controls.minDistance = 1.5;
-// controls.maxDistance = 10;
+const outputPass = new OutputPass();
 
-// Target for computing the water refraction
-
+const composer = new EffectComposer( renderer );
+composer.addPass( renderScene );
+composer.addPass( bloomPass );
+composer.addPass( outputPass );
 // Clock
 const clock = new THREE.Clock();
 
@@ -237,6 +244,211 @@ const coralLoaded = new Promise((resolve, reject) => {
     });
   });
 
+let animationClips = [];
+
+let monsterMesh = new THREE.Mesh();
+let clownfishMesh;
+let bluetangMesh = new THREE.Mesh();
+let brownfishMesh = new THREE.Mesh();
+let tunaMesh = new THREE.Mesh();
+
+const monsterLoaded = new Promise((resolve, reject) => {
+    gtlfLoader.load('models/fish/monster.glb'
+  ,function ( gltf ) {
+    monsterMesh = gltf.scene;
+    monsterMesh.position.set(0, 0, 0);
+    monsterMesh.rotation.set(Math.PI / 2, 0, 0);
+    monsterMesh.scale.set(5, 5, 5);
+    monsterMesh.castShadow = true;
+    monsterMesh.name = 'Monster';
+
+    var monster = clone(monsterMesh);
+    monster.position.set(0, 0, -5);
+    monster.castShadow = true;
+    //scene.add(monster);
+    //scene.add(monsterMesh);
+      //scene.add(model);
+  },
+
+  // called while loading is progressing
+  function ( xhr ) {
+
+      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+      resolve();
+
+  },
+  // called when loading has errors
+  function ( error ) {
+
+      console.log( 'An error happened ' + error.message);
+      reject();
+
+  });
+});
+
+const clownFishLoaded = new Promise((resolve, reject) => {
+  gtlfLoader.load('models/fish/clownfish.glb'
+,function ( gltf ) {
+    clownfishMesh = gltf.scene;
+    animationClips = gltf.animations;
+    
+    clownfishMesh.position.set(0, 0, 2);
+    clownfishMesh.rotation.set(Math.PI / 2, 0, 0);
+    clownfishMesh.scale.set(.08, .08, .08);
+    clownfishMesh.name = 'clownfish';
+    clownfishMesh.traverse( function( child ) { 
+
+      if ( child.isMesh ) {
+  
+          child.castShadow = true;
+          child.receiveShadow = true;
+  
+      }
+  
+  } );
+    
+    createClownFish(7);
+    
+},
+
+// called while loading is progressing
+function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    resolve();
+
+},
+// called when loading has errors
+function ( error ) {
+
+    console.log( 'An error happened ' + error.message);
+    reject();
+
+});
+});
+
+const blueTangFishLoaded = new Promise((resolve, reject) => {
+  gtlfLoader.load('models/fish/bluetang.glb'
+,function ( gltf ) {
+    bluetangMesh = gltf.scene;
+    bluetangMesh.position.set(0, 0, 2);
+    bluetangMesh.rotation.set(Math.PI / 2, 0, 0);
+    bluetangMesh.scale.set(.05, .05, .05);
+    bluetangMesh.name = 'bluetang';
+    bluetangMesh.traverse( function( child ) { 
+
+      if ( child.isMesh ) {
+  
+          child.castShadow = true;
+          child.receiveShadow = true;
+  
+      }
+  
+  } );
+    createBlueTangFish(7, new THREE.Vector3(0, 0, 0), 1);
+},
+
+// called while loading is progressing
+function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    resolve();
+
+},
+// called when loading has errors
+function ( error ) {
+
+    console.log( 'An error happened ' + error.message);
+    reject();
+
+},
+
+// called while loading is progressing
+function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    resolve();
+
+},
+// called when loading has errors
+function ( error ) {
+
+    console.log( 'An error happened ' + error.message);
+    reject();
+
+});
+});
+
+const tunaFishLoaded = new Promise((resolve, reject) => {
+  gtlfLoader.load('models/fish/tuna.glb'
+,function ( gltf ) {
+    tunaMesh = gltf.scene;
+    tunaMesh.position.set(0, 0, 2);
+    tunaMesh.rotation.set(Math.PI / 2, 0, 0);
+    tunaMesh.scale.set(.2, .2, .2);
+    tunaMesh.name = 'tuna';
+    tunaMesh.traverse( function( child ) { 
+
+      if ( child.isMesh ) {
+  
+          child.castShadow = true;
+          child.receiveShadow = true;
+  
+      }
+  
+  } );
+    createTunaFish(3, 5);
+},
+
+// called while loading is progressing
+function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    resolve();
+
+},
+// called when loading has errors
+function ( error ) {
+
+    console.log( 'An error happened ' + error.message);
+    reject();
+
+});
+});
+
+const brownFishLoaded = new Promise((resolve, reject) => {
+  gtlfLoader.load('models/fish/brownfish.glb'
+,function ( gltf ) {
+    const model = gltf.scene;
+    model.traverse( function( child ) {
+      if (child instanceof THREE.Mesh) {
+        gltf.scene.position.set(0, 0, 2);
+        model.rotation.set(Math.PI / 2, 0, 0);
+        model.scale.set(.1, .1, .1);
+        model.name = 'brownfish';
+        brownfishMesh = model;
+      }
+    });
+    //brownfishMesh = model;
+},
+
+// called while loading is progressing
+function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    resolve();
+
+},
+// called when loading has errors
+function ( error ) {
+
+    console.log( 'An error happened ' + error.message);
+    reject();
+
+});
+});
+
+
 
 
 // Skybox
@@ -248,7 +460,7 @@ const skybox = cubetextureloader.load([
   'assets/TropicalSunnyDay_pz.jpg', 'assets/TropicalSunnyDay_nz.jpg',
 ]);
 
-scene.background = skybox;
+//scene.background = skybox;
 
 const textureLoader = new THREE.TextureLoader();
 const sand_texture = textureLoader.load('textures/sand_texture_2.jpg' ); 
@@ -256,6 +468,8 @@ const sand_texture = textureLoader.load('textures/sand_texture_2.jpg' );
 const white_texture = textureLoader.load('textures/caustics_texture.png' );
 const caustics_texture = textureLoader.load('textures/CausticsRender_003.png' );
 
+spotLight.map = sand_texture;
+spotLight.castShadow = true;
 // immediately use the texture for material creation 
 
 const material = new THREE.MeshStandardMaterial( { map: sand_texture } );
@@ -264,27 +478,26 @@ const planeGeometry = new THREE.PlaneGeometry(10, 10);
 const planeMesh = new THREE.Mesh(planeGeometry, material);
 planeMesh.position.set(0, 0, 0);
 planeMesh.receiveShadow = true;
-
 //causticsScene.add(planeMesh);
 scene.add(planeMesh);
 
-const materialCaustics = new THREE.MeshStandardMaterial( { map: caustics_texture, transparent: true, alphaTest: 0.7});
+const materialCaustics = new THREE.MeshStandardMaterial( {  map: caustics_texture});
 const causticsMesh = new THREE.Mesh(planeGeometry, materialCaustics);
-  causticsMesh.position.set(5, 0, 5);
-  causticsScene.add(causticsMesh);
+causticsMesh.position.set(5, 0, 5);
+causticsScene.add(causticsMesh);
 
-  const causticsMesh2 = new THREE.Mesh(planeGeometry, materialCaustics);
-  causticsMesh2.position.set(-5, 0, 5);
-  causticsScene.add(causticsMesh2);
-
-  spotLight.map = temporaryRenderTarget.texture;
-
-//causticsMesh.castShadow = true;
-//scene.add(causticsMesh);
+const causticsMesh2 = new THREE.Mesh(planeGeometry, materialCaustics);
+causticsMesh2.position.set(-5, 0, 5);
+causticsScene.add(causticsMesh2);
 
 
+//causticsMesh.castShadow = true
 var count = 1;
 
+const rotationMatrix = new THREE.Matrix4();
+const targetQuaternion = new THREE.Quaternion();
+
+const fishGroup = [];
 // Main rendering loop
 function animate() {
   stats.begin();
@@ -292,13 +505,41 @@ function animate() {
   renderer.setRenderTarget(temporaryRenderTarget);
   
   renderer.clear();
+  renderer.setClearColor(0x194df7);
   renderer.render(causticsScene, causticsCamera);
 
   renderer.setRenderTarget(null);
   renderer.clear();
-
+  renderer.setClearColor(0x194df7);
   //renderer.render(causticsScene, causticsCamera);
   renderer.render(scene, camera);
+  const delta = clock.getDelta();
+  fishGroup.forEach(fish => {
+
+    if (fish.name == 'Monster') {
+      // fish.fishMesh.position.x = Math.sin(clock.getElapsedTime() * 2 * Math.PI * fish.speed) * fish.radius;
+      // fish.fishMesh.position.y = Math.cos(clock.getElapsedTime() * 2 * Math.PI * fish.speed) * fish.radius;
+    }
+    else {
+      if (fish.fishMesh.position.distanceTo(fish.destPos) < 0.1) {
+        fish.setDestPos();
+      }
+      var dir = new THREE.Vector3(); 
+      const step = delta;
+      dir.subVectors( fish.destPos, fish.fishMesh.position ).normalize();
+      fish.fishMesh.position.x += dir.x * step * fish.speed;
+      fish.fishMesh.position.y += dir.y * step * fish.speed;
+      fish.fishMesh.position.z += dir.z * step * fish.speed;
+      rotationMatrix.lookAt( fish.fishMesh.position, fish.destPos, new THREE.Vector3(0, 0, 1) );
+      targetQuaternion.setFromRotationMatrix( rotationMatrix );
+      fish.fishMesh.quaternion.rotateTowards( targetQuaternion, 5 * delta );
+    }
+
+    fish.mixer.update(delta);
+    //fish.fishMesh.lookAt(new THREE.Vector3(-fish.destPos.x, fish.destPos.y, fish.destPos.z));
+  });
+  //composer.render();
+  //renderer.render(scene, camera)
 
   controls.update();
   TWEEN.update();
@@ -307,20 +548,6 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
-// function onMouseMove(event) {
-//   const rect = canvas.getBoundingClientRect();
-
-//   mouse.x = (event.clientX - rect.left) * 2 / width - 1;
-//   mouse.y = - (event.clientY - rect.top) * 2 / height + 1;
-
-//   raycaster.setFromCamera(mouse, camera);
-
-//   const intersects = raycaster.intersectObject(targetmesh);
-
-//   for (let intersect of intersects) {
-//     waterSimulation.addDrop(renderer, intersect.point.x, intersect.point.y, 0.03, 0.02);
-//   }
-// }
 
 const loaded = [
   // waterSimulation.loaded,
@@ -332,6 +559,11 @@ const loaded = [
   // sharkLoaded,
   // rockLoaded,
   // plantLoaded,
+  clownFishLoaded,
+  monsterLoaded,
+  blueTangFishLoaded,
+  brownFishLoaded,
+  tunaFishLoaded,
   coralLoaded
 ];
 
@@ -355,34 +587,114 @@ const loaded = [
 //   }
 // }
 
-Promise.all(loaded).then(() => {
-  // envGeometries.push(floorGeometry);
-  // envGeometries.push(shark);
-  // envGeometries.push(rock1);
-  // envGeometries.push(rock2);
-
-  // //const envGeometries = [floorGeometry, shark, rock1, rock2, plant, coralreef];
-  // // group.children.forEach(child => {
-  // //   envGeometries.push(child.geometry);
-  // // });
-
-  // environmentMap.setGeometries(envGeometries);
-  // environment.setGeometries(envGeometries);
-
-  // environment.addTo(scene);
-  // scene.add(water.mesh);
-
-  // caustics.setDeltaEnvTexture(1. / environmentMap.size);
-
-  // //canvas.addEventListener('mousemove', { handleEvent: onMouseMove });
-
-  // for (var i = 0; i < 5; i++) {
-  //   waterSimulation.addDrop(
-  //     renderer,
-  //     Math.random() * 2 - 1, Math.random() * 2 - 1,
-  //     0.03, (i & 1) ? 0.02 : -0.02
-  //   );
-  // }
-
+await Promise.all(loaded).then(() => {
+  //createClownFish(5);
+  console.log("Done");
   animate();
 });
+
+function createClownFish(numberOfFish) {
+  for (var i = 0; i < numberOfFish; i++) {
+    var clownFish = clone(clownfishMesh);
+    clownFish.position.set(getRandomInt(-4, 5), getRandomInt(-4, 5), getRandomInt(1, 4));
+    clownFish.castShadow = true;
+    scene.add(clownFish);
+    var fish = new Fish(clownFish, 'Clownfish', .25);
+    fish.setDestPos();
+    fishGroup.push(fish);
+    const mixer = new THREE.AnimationMixer( clownFish );
+    fish.setMixer(mixer);
+
+
+    const clip = THREE.AnimationClip.findByName( animationClips, 'ClownFish' );
+    const action = mixer.clipAction( clip );
+    action.play();
+  }
+}
+
+function createBlueTangFish(numberOfFish, centerPos, radius) {
+  for (var i = 0; i < numberOfFish; i++) {
+    var bluetang = clone(bluetangMesh);
+    bluetang.position.set(centerPos.x + getRandomInt(-radius, radius + 1), 
+      centerPos.y + getRandomInt(-radius, radius + 1), 
+      centerPos.z + getRandomInt(-radius, radius + 1));
+    bluetang.castShadow = true;
+    scene.add(bluetang);
+    var fish = new Fish(bluetang, 'Bluetang', .8, centerPos, radius);
+    fish.setDestPos();
+    fishGroup.push(fish);
+    const mixer = new THREE.AnimationMixer( bluetang );
+    fish.setMixer(mixer);
+
+
+    const clip = THREE.AnimationClip.findByName( animationClips, 'DorySwim' );
+    const action = mixer.clipAction( clip );
+    action.play();
+  }
+}
+
+function createTunaFish(numberOfFish, radius) {
+  for (var i = 0; i < numberOfFish; i++) {
+    var tuna = clone(tunaMesh);
+    tuna.position.set(getRandomInt(-radius, radius + 1), 
+          getRandomInt(-radius, radius + 1), 
+          getRandomInt(7, 10));
+    tuna.castShadow = true;
+    scene.add(tuna);
+    var fish = new Fish();
+    fish.name = 'Tuna';
+    fish.fishMesh = tuna;
+    fish.speed = .2;
+    fish.radius = radius;
+    fish.setDestPos();
+    fishGroup.push(fish);
+    const mixer = new THREE.AnimationMixer( tuna );
+    fish.setMixer(mixer);
+
+    const clip = THREE.AnimationClip.findByName( animationClips, 'TunaSwim' );
+    const action = mixer.clipAction( clip );
+    action.play();
+  }
+}
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+class Fish {
+  constructor(fishMesh, name, speed, centerPos, radius) {
+    this.fishMesh = fishMesh;
+    this.speed = speed;
+    this.centerPos = centerPos;
+    this.radius = radius;
+    this.name = name;
+  }
+
+  setDestPos() {
+    var destPos;
+    switch (this.name) {
+      case 'Clownfish':
+        destPos = new THREE.Vector3(getRandomInt(-4, 5), getRandomInt(-4, 5), getRandomInt(0, 4));
+        this.destPos = destPos;
+        break;
+      case 'Bluetang':
+        destPos = new THREE.Vector3(this.centerPos.x + getRandomInt(-this.radius, this.radius + 1), 
+          this.centerPos.y + getRandomInt(-this.radius, this.radius + 1), 
+          this.centerPos.z + getRandomInt(this.centerPos.z, this.radius + 1));
+        this.destPos = destPos;
+        break;
+      case 'Tuna':
+        var multiplier = this.fishMesh.position > 0 ? -1 : 1;
+        destPos = new THREE.Vector3(multiplier * getRandomInt(3, 6), multiplier * getRandomInt(3, 6), this.fishMesh.position.z);
+        this.destPos = destPos;
+        break;
+    }
+  }
+
+  setMixer(mixer) {
+    this.mixer = mixer;
+  }
+}
